f73d5a89fa13f336ebe9e66ae7ab6ce2d4ca04d6
vdrabll <fedosova.univ@gmail.com>
2024-01-28 14:08:52 +0300

    cinda get used to arrays and stream api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Task2 task1 = new Task2();
         int[] testArray = new int[]{1, 2, 4, 66, 6, 7, 66, 3, 1};
-        task1.clearArray(testArray, 66);
 
         // todo: you can write your own test code
     }
